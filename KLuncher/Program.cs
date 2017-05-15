e3ca23a7a99210f3ab150554ecb59b597c896614
Mariusz Brzeski <marianob85work@gmail.com>
2017-05-15 17:54:24 +0200

    Init

---
+++ KLuncher/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace KLuncher
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+
+            // Show the system tray icon.					
+            using (NotifyIcon pi = new NotifyIcon())
+            {
+                pi.Visible = true;
+
+                // Make sure the application runs!
+                Application.Run();
+            }
+        }
+    }
+}
