66d5082cc7187f4b2b4d3603c1a4c48a2624f325
Mariusz Brzeski <mbrzeski@atsisa.com>
2017-05-18 11:56:22 +0200

    Modified   KLuncher/KeyboardHook.cs Modified   KLuncher/Program.cs

--- KLuncher/Program.cs
+++ KLuncher/Program.cs
@@ -21,17 +21,36 @@ namespace KLuncher
 
             AllocConsole();
 
+            System.Timers.Timer startTimer = new System.Timers.Timer(5000);
+            startTimer.Elapsed += async (sender, e) => await HandleTimer(sender);
+            //startTimer.Elapsed += OnTimedEvent;
+            startTimer.Start();
+
             // Show the system tray icon.					
-            using( KeyboardHook kh = new KeyboardHook())
+            using(KeyboardHook kh = new KeyboardHook())
             using (ProcessIcon pi = new ProcessIcon())
             {
                 pi.Display();
 
+                Console.WriteLine("App started");
                 // Make sure the application runs!
                 Application.Run();
             }
         }
+        private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("\nHandler not implemented...");
+        }
 
+        private static Task HandleTimer(Object source)
+        {
+            System.Timers.Timer timer = source as System.Timers.Timer;
+            timer.Close();
+
+            Console.WriteLine("\nHandler not implemented...");
+            Task taskA = new Task(() => Console.WriteLine("Hello from taskA."));
+            return taskA;
+        }
 
         [DllImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
