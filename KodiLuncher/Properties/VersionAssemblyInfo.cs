0469870356431b942672d3c9c18475562364ca71
Mariusz Brzeski <marianob85work@gmail.com>
2017-05-23 17:35:06 +0200

    Modified   KodiLuncher/KodiLuncher.csproj Modified   KodiLuncher/Properties/VersionAssemblyInfo.cs Add        KodiLuncher/Tray.ico

--- KodiLuncher/Properties/VersionAssemblyInfo.cs
+++ KodiLuncher/Properties/VersionAssemblyInfo.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0.0 (git 96ac9c32aa2640a470de0e5ce8b7c9d5d3f38b81)")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0.0 (git cbec9c2090b648b09cab3cd14414fc1a36fb12d8)")]
 [assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
 [assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
 
